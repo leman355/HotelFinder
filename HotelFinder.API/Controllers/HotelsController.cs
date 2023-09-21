52d4abaf53b70909c541b603923d63330d80ae13
leman355 <leman355@gmail.com>
2023-09-21 23:09:27 +0400

    HotelFinder

---
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -0,0 +1,46 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace HotelFinder.API.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class HotelsController : Controller
+    {
+        private IHotelService _hotelService;
+
+        public HotelsController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+
+        }
+        [HttpGet]
+        public List<Hotel> Get()
+        {
+            return _hotelService.GetAllHotels();
+        }
+        [HttpGet("{id}")]
+        public Hotel Get(int id)
+        {
+            return _hotelService.GetHotelById(id);
+        }
+        [HttpPost]
+        public Hotel Post(Hotel hotel)
+        {
+            return _hotelService.CreateHotel(hotel);
+        }
+        [HttpPut]
+        public Hotel Put(Hotel hotel)
+        {
+            return _hotelService.UpdateHotel(hotel);
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _hotelService.DeleteHotel(id);
+        }
+    }
+}
---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=HotelDb;User Id=postgres;Password=1;Search Path=public");
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
